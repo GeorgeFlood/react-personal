e74b2fc9cb6122be6c84beb6c68a61e1baab54f8
GeorgeFlood <mrgeorgeflood@gmail.com>
2023-05-08 14:19:20 +0100

    changed vite.config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,18 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react({ include: /\.(mjs|js|ts|tsx)$/ })],
+  plugins: [react()],
   base: process.env.NODE_ENV === "production" ? "/react-personal/" : "/",
+  build: {
+    target: "es2015",
+    outDir: "dist",
+    assetsDir: "assets",
+    rollupOptions: {
+      output: {
+        entryFileNames: "[name].js",
+        chunkFileNames: "[name].js",
+        assetFileNames: "[name].[ext]",
+      },
+    },
+  },
 });
