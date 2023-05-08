2daab606de6b7ca4d7fedb105f87ba97fe47e3c7
George <67131869+GeorgeFlood@users.noreply.github.com>
2023-05-08 12:37:11 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,23 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// Custom plugin to set the MIME type for .jsx files
+const customJSXPlugin = {
+  name: 'jsx-mime-type',
+  transform(_, id) {
+    if (id.endsWith('.jsx')) {
+      return {
+        code: _,
+        map: null,
+        moduleSideEffects: true,
+        loader: 'js',
+      };
+    }
+  },
+};
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: process.env.NODE_ENV === "production" ? "/react-personal/" : "/",
-  server: {
-    mimeTypes: {
-      'jsx': 'application/javascript'
-    }
-  }
+  plugins: [react(), customJSXPlugin],
+  base: process.env.NODE_ENV === 'production' ? '/react-personal/' : '/',
 });
