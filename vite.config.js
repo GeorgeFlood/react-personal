aa953a3f0dbdd04e99cc8bbbb288927212c95423
GeorgeFlood <mrgeorgeflood@gmail.com>
2023-05-08 14:48:43 +0100

    changed vite cofig once again

--- vite.config.js
+++ vite.config.js
@@ -3,18 +3,5 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
   base: process.env.NODE_ENV === "production" ? "/react-personal/" : "/",
-  build: {
-    target: "es2015",
-    outDir: "dist",
-    assetsDir: "assets",
-    rollupOptions: {
-      output: {
-        entryFileNames: "[name].js",
-        chunkFileNames: "[name].js",
-        assetFileNames: "[name].[ext]",
-      },
-    },
-  },
 });
