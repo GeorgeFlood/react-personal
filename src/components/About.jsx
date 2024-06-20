8f3aaca710ca0cf27758ef84138c8f50e40f4bb2
GeorgeFlood <mrgeorgeflood@gmail.com>
2024-06-20 20:22:19 +0100

    CV change
    cv change and about me background change

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,7 +2,7 @@ import { useRef } from "react";
 import myImg from "../../Images/aboutme.jpg";
 import download from "../../Images/download.svg";
 import useOnScreen from "./hooks/UseOnScreen.jsx";
-const cvUrl = "/George_Flood_CV.docx";
+const cvUrl = "/GeorgeFlood_CV.docx";
 
 const About = () => {
   const aboutRef = useRef(null);
